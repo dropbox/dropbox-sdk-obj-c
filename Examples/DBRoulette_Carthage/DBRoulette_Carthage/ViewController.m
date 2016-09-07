e566bcc30768d855270b711d0b2f39fd0786cfca
Stephen Cobbe <scobbe@dropbox.com>
2016-09-06 21:41:43 -0700

    A few small changes.

--- Examples/DBRoulette_Carthage/DBRoulette_Carthage/ViewController.m
+++ Examples/DBRoulette_Carthage/DBRoulette_Carthage/ViewController.m
@@ -6,9 +6,12 @@
 //  Copyright © 2016 Dropbox. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "ViewController.h"
 
 #import <ObjectiveDropboxOfficial/ObjectiveDropboxOfficial.h>
+#import <ObjectiveDropboxOfficial/DropboxClient.h>
 
 @interface ViewController ()
 
@@ -18,6 +21,8 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  DropboxClient *client = [[DropboxClient alloc] initWithAccessToken:@"test"];
+  NSLog(@"%@", client);
   
   // Do any additional setup after loading the view, typically from a nib.
 }
