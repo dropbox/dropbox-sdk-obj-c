2f421fc817999c6d0d5b87eb4f6c1d4801f7da7f
Stephen Cobbe <scobbe@dropbox.com>
2017-01-11 13:02:47 -0800

    Added Xplat umbrella.

---
+++ Source/ObjectiveDropboxOfficial/Shared/Handwritten/ObjectiveDropboxOfficialXplat.h
@@ -0,0 +1,11 @@
+///
+/// Copyright (c) 2016 Dropbox, Inc. All rights reserved.
+///
+
+#import "DropboxSDKImportsShared.h"
+
+#if TARGET_OS_IPHONE
+#import "DropboxSDKImports-iOS.h"
+#else
+#import "DropboxSDKImports-macOS.h"
+#endif
