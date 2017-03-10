18053ac309446082362e15d2cee0125b4e0d7a98
Stephen Cobbe <scobbe@dropbox.com>
2017-03-09 17:26:52 -0800

    Added pragma unused.

--- Source/ObjectiveDropboxOfficial/Platform/ObjectiveDropboxOfficial_iOS/DBOAuthMobile-iOS.m
+++ Source/ObjectiveDropboxOfficial/Platform/ObjectiveDropboxOfficial_iOS/DBOAuthMobile-iOS.m
@@ -212,6 +212,9 @@
     createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration
                forNavigationAction:(WKNavigationAction *)navigationAction
                     windowFeatures:(WKWindowFeatures *)windowFeatures {
+#pragma unused(webView)
+#pragma unused(configuration)
+#pragma unused(windowFeatures)
   // For target="_bank" urls, we want to suppress the call, then reopen in new browser
   if (!navigationAction.targetFrame.isMainFrame) {
     _tryInterceptHandler(navigationAction.request.URL, YES);
