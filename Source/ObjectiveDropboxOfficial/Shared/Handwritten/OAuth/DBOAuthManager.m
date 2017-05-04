13d17f2298956829b3befd7f2801451cfb25bb0e
Stephen Cobbe <scobbe@dropbox.com>
2017-05-04 13:25:30 -0700

    Allow signup button on macOS.

--- Source/ObjectiveDropboxOfficial/Shared/Handwritten/OAuth/DBOAuthManager.m
+++ Source/ObjectiveDropboxOfficial/Shared/Handwritten/OAuth/DBOAuthManager.m
@@ -60,7 +60,11 @@ static DBOAuthManager *s_sharedOAuthManager;
     _cancelURL = [NSURL URLWithString:[NSString stringWithFormat:@"db-%@://2/cancel", _appKey]];
     _host = host;
     _urls = [NSMutableArray arrayWithObjects:_redirectURL, nil];
+#ifdef TARGET_OS_MAC
+    _disableSignup = NO;
+#else
     _disableSignup = YES;
+#endif
   }
   return self;
 }
