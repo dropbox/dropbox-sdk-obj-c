81455bb46ff00bc17cb4a02c1beabdc20321d502
Stephen Cobbe <scobbe@dropbox.com>
2018-02-28 12:25:03 -0800

    Fix timeout issue.

--- Source/ObjectiveDropboxOfficial/Shared/Handwritten/Networking/DBTransportDefaultClient.m
+++ Source/ObjectiveDropboxOfficial/Shared/Handwritten/Networking/DBTransportDefaultClient.m
@@ -98,7 +98,7 @@
   NSURLSession *sessionToUse = _session;
 
   // longpoll requests have a much longer timeout period than other requests
-  if ([route class] == [DBFILESRouteObjects.DBFILESListFolderLongpoll class]) {
+  if ([route.attrs[@"host"] isEqualToString:@"notify"]) {
     sessionToUse = _longpollSession;
   }
 
