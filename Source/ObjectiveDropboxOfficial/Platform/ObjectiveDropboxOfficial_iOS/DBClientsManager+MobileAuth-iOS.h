db7d5e6ceb51fd6f1981015173a86cbb1fa57c85
Xuyan Ke <xuke@dropbox.com>
2020-06-04 16:52:45 -0700

    Added support to show loading status for the last step of OAuth code flow
    OAuth code flow requires a network request to get access token, apps should build some UX to handle this async operation.
    A default UX is built in the SDK with showing a load spinner while waiting for oauth token request to finish.
    Apps can provide a `DBLoadingStatusDelegate` to override the default and show some custom experience.

--- Source/ObjectiveDropboxOfficial/Platform/ObjectiveDropboxOfficial_iOS/DBClientsManager+MobileAuth-iOS.h
+++ Source/ObjectiveDropboxOfficial/Platform/ObjectiveDropboxOfficial_iOS/DBClientsManager+MobileAuth-iOS.h
@@ -11,6 +11,8 @@
 @class UIApplication;
 @class UIViewController;
 
+@protocol DBLoadingStatusDelegate;
+
 NS_ASSUME_NONNULL_BEGIN
 
 ///
@@ -47,11 +49,15 @@ NS_ASSUME_NONNULL_BEGIN
 /// @param sharedApplication The `UIApplication` with which to render the OAuth flow.
 /// @param controller The `UIViewController` with which to render the OAuth flow. Please ensure that this is the
 /// top-most view controller, so that the authorization view displays correctly.
+/// @param loadingStatusDelegate An optional delegate to handle loading experience during auth flow.
+/// e.g. Show a looading spinner and block user interaction while loading/waiting.
+/// If a delegate is not provided, the SDK will show a default loading spinner when necessary.
 /// @param openURL A wrapper around app-extension unsafe `openURL` call.
 /// @param scopeRequest Request contains information of scopes to be obtained.
 ///
 + (void)authorizeFromControllerV2:(UIApplication *)sharedApplication
                        controller:(nullable UIViewController *)controller
+            loadingStatusDelegate:(nullable id<DBLoadingStatusDelegate>)loadingStatusDelegate
                           openURL:(void (^_Nonnull)(NSURL *))openURL
                      scopeRequest:(nullable DBScopeRequest*)scopeRequest;
 
