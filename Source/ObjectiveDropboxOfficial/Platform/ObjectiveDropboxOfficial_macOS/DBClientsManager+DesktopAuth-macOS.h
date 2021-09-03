6d0a29d77fdc80f3addb717bf9db4e4f7c97c69a
Julian Locke <jlocke@dropbox.com>
2021-09-03 14:25:14 -0400

    Regenerate route objects

--- Source/ObjectiveDropboxOfficial/Platform/ObjectiveDropboxOfficial_macOS/DBClientsManager+DesktopAuth-macOS.h
+++ Source/ObjectiveDropboxOfficial/Platform/ObjectiveDropboxOfficial_macOS/DBClientsManager+DesktopAuth-macOS.h
@@ -28,7 +28,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// Commences OAuth desktop flow from supplied view controller.
 ///
 /// This method should no longer be used.
-/// Long-lived access tokens are deprecated. See https://dropbox.tech/developers/migrating-app-permissions-and-access-tokens.
+/// Long-lived access tokens are deprecated. See
+/// https://dropbox.tech/developers/migrating-app-permissions-and-access-tokens.
 /// Please use `authorizeFromControllerDesktopV2` instead.
 ///
 /// @param sharedApplication The `NSWorkspace` with which to render the OAuth flow.
@@ -38,7 +39,9 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 + (void)authorizeFromControllerDesktop:(NSWorkspace *)sharedApplication
                             controller:(nullable NSViewController *)controller
-                               openURL:(void (^_Nonnull)(NSURL *))openURL __deprecated_msg("This method was used for long-lived access tokens, which are now deprecated. Please use `authorizeFromControllerDesktopV2` instead.");
+                               openURL:(void (^_Nonnull)(NSURL *))openURL
+    __deprecated_msg("This method was used for long-lived access tokens, which are now deprecated. Please use "
+                     "`authorizeFromControllerDesktopV2` instead.");
 
 ///
 /// Commences OAuth mobile flow from supplied view controller.
