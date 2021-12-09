2c9997be7370b783a7041e12dd49e0862fda83dd
Taylor Case <tcase@dropbox.com>
2021-12-09 13:07:00 -0800

    6.2.3 release.

--- Source/ObjectiveDropboxOfficial/Shared/Handwritten/Resources/DBSDKConstants.m
+++ Source/ObjectiveDropboxOfficial/Shared/Handwritten/Resources/DBSDKConstants.m
@@ -8,7 +8,7 @@
 
 #import "DBSDKConstants.h"
 
-NSString *const kDBSDKVersion = @"6.2.2";
+NSString *const kDBSDKVersion = @"6.2.3";
 NSString *const kDBSDKDefaultUserAgentPrefix = @"OfficialDropboxObjCSDKv2";
 NSString *const kDBSDKForegroundSessionId = @"com.dropbox.dropbox_sdk_obj_c_foreground";
 NSString *const kDBSDKBackgroundSessionId = @"com.dropbox.dropbox_sdk_obj_c_background";
