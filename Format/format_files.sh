7f7a25101298aa11d584699d5e6b5c38d6af95e8
Julian Locke <jlocke@dropbox.com>
2023-07-21 16:51:29 -0400

    Remove bundled clang-format

--- Format/format_files.sh
+++ Format/format_files.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+
 # e.g. `format_files.sh <srcs_path>`
 #
 
-srcs_path=$1
+set -euo pipefail
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    find "$srcs_path" -type f -name "*[.h|.m]" | xargs ./clang-format -i -style=file
+exit_with_message() {
+    local message=$1
+    local retcode="${2:-0}"
+
+    echo 1>&2 "$message"
+    exit "$retcode"
+}
+
+exit_with_clang_format_install_message() {
+    exit_with_message "Skipping code formatting. Please install clang-format version 16 or greater: 'brew install clang-format'"
+}
+
+if ! [ -x "$(command -v clang-format)" ]; then
+    exit_with_clang_format_install_message
 fi
+
+if ! [ "$(uname -s)" == "Darwin" ]; then
+    exit 0
+fi
+
+clang_format_version="$(clang-format --version | cut -f3 -w | tail)"
+clang_format_version_major=$(awk -F. '{print $1}' <<<"$clang_format_version")
+
+if [ "$clang_format_version_major" -lt 16 ]; then
+    exit_with_clang_format_install_message
+fi
+
+srcs_path=$1
+find "$srcs_path" -type f -name "*[.h|.m]" -exec clang-format -i -style=file "{}" \;
